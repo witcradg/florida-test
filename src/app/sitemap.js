a1db876e9273afe4624dfcee66c8d98a86c6d083
witcradg <witcradg@gmail.com>
2024-02-24 10:53:36 -0800

    Remove cities from sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -2,7 +2,7 @@ import { allMdxPages } from "contentlayer/generated"
 import { allProducts } from "contentlayer/generated"
 import { headers } from "next/headers"
 import { globby } from "globby"
-import { getAllCities } from "../lib/citiesCsv"
+// import { getAllCities } from "../lib/citiesCsv"
 
 export default async function sitemap() {
   const headersList = headers()
@@ -25,18 +25,18 @@ export default async function sitemap() {
     }
   })
 
-  const allCities = await getAllCities()
-  const cityPages = allCities.map((city) => {
-    const slug = city[3].toLowerCase().replace(/ /g, "-")
-    return {
-      url: `${baseUrl}/florida/${slug}`,
-      lastModified: new Date().toISOString(),
-    }
-  })
+  // const allCities = await getAllCities()
+  // const cityPages = allCities.map((city) => {
+  //   const slug = city[3].toLowerCase().replace(/ /g, "-")
+  //   return {
+  //     url: `${baseUrl}/florida/${slug}`,
+  //     lastModified: new Date().toISOString(),
+  //   }
+  // })
 
   //Add the static pages to the sitemap
   const pages = await globby("src/app/**/page.js")
-  console.log("pages", pages)
+  // console.log("pages", pages)
   const rawPages = pages.map((page) => {
     const url = page
       .replace("/page.js", "")
@@ -57,7 +57,6 @@ export default async function sitemap() {
       url: home,
       lastModified: new Date().toISOString(),
     },
-    ...cityPages,
     ...mdxPages,
     ...productPages,
     ...staticPages,
